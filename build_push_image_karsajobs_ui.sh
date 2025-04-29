2dc81a188a88ec22b2b7f28c32f114443add1830
Wahyu Hidayat <wahyu.hidayat3@gmail.com>
2025-04-29 10:46:34 +0700

    Menambah file build karsajobs-ui sesuai kriteria submission 2

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variabel
+USERNAME=hidawaa
+REPO_NAME=karsajobs-ui
+REGISTRY=ghcr.io
+IMAGE_NAME=$REGISTRY/$USERNAME/$REPO_NAME:latest
+
+# Build image di lokal
+echo "Build image..."
+docker build -t $IMAGE_NAME .
+
+# Login GitHub Packages
+echo "Login..."
+echo $TOKEN | docker login ghcr.io -u $USERNAME --password-stdin
+
+# Upload Image
+echo "Upload image..."
+docker push $IMAGE_NAME
+
+echo "Berhasil upload image $IMAGE_NAME"
