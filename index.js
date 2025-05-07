18ed2e7f9e99dc72b6820a2dcb8f64595663d4d4
Wahyu Hidayat <wahyu.hidayat3@gmail.com>
2025-05-07 15:17:06 +0700

    Menambah komentar di file index.js

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-require('dotenv').config()
+// Load konfigurasi dari .env
+require("dotenv").config();
 
+// Import Express.js
 const express = require("express");
+
+// Instance Express
 const app = express();
 
+// Import body-parser
 const bp = require("body-parser");
 
+// Impor amqplib
 const amqp = require("amqplib");
+
+// Mengambil AMQP_URL dari environment variable
 const amqpServer = process.env.AMQP_URL;
+
+// Delkarasi variabel
 var channel, connection;
 
+// Memanggil fungsi koneksi ke RabbitMQ
 connectToQueue();
 
+// Fungsi koneksi ke RabbitMQ
 async function connectToQueue() {
-    try {
-        connection = await amqp.connect(amqpServer);
-        channel = await connection.createChannel();
-        await channel.assertQueue("order");
-        channel.consume("order", data => {
-            console.log(`Order received: ${Buffer.from(data.content)}`);
-            console.log("** Will be shipped soon! **\n")
-            channel.ack(data);
-        });
-    } catch (ex) {
-        console.error(ex);
-    }
+  // Blok Try Catch
+  try {
+    // Membuat koneksi ke RabbitMQ
+    connection = await amqp.connect(amqpServer);
+
+    // Membuat channel komunikasi
+    channel = await connection.createChannel();
+
+    // Membuat antrian jika belum ada
+    await channel.assertQueue("order");
+
+    // Handle pesan dari antrein "order"
+    channel.consume("order", (data) => {
+      // Menampilkan data order yang diterima
+      console.log(`Order received: ${Buffer.from(data.content)}`);
+
+      // Menampilkan pesan order akan segera dikirim
+      console.log("** Will be shipped soon! **\n");
+
+      //Memberi tanda pesan telah diproses
+      channel.ack(data);
+    });
+
+    // Catch
+  } catch (ex) {
+    // Menampilkan error ketika gagal
+    console.error(ex);
+  }
 }
 
+// Menjalankan server Express pada port sesuai environment variable
 app.listen(process.env.PORT, () => {
-    console.log(`Server running at ${process.env.PORT}`);
+  // Menampilkan pesan server telah berjalan pada port sesuai environment variable
+  console.log(`Server running at ${process.env.PORT}`);
 });
